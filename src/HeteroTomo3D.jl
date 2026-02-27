06b56c4268b6b6fc0d62fb9a8951c7dfac37d601
YUN Ho <ho.yun@epfl.ch>
2026-02-27 13:25:48 +0100

    Add dummy_volume function and unit tests

--- src/HeteroTomo3D.jl
+++ src/HeteroTomo3D.jl
@@ -1,5 +1,15 @@
 module HeteroTomo3D
 
-# Write your package code here.
+# Exporting makes the function available when someone types `using HeteroTomo3D`
+export dummy_volume
+
+"""
+    dummy_volume(width, height, depth)
+
+Calculates the volume of a simple 3D bounding box.
+"""
+function dummy_volume(width, height, depth)
+    return width * height * depth
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using HeteroTomo3D
 using Test
 
 @testset "HeteroTomo3D.jl" begin
-    # Write your tests here.
+    # Test our dummy volume function
+    @test dummy_volume(2, 3, 4) == 24
+
+    # You can add more tests here later
+    @test dummy_volume(1, 1, 1) == 1
 end

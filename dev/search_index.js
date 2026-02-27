d5b75983e3d9d75edb749f45ce7898efb46cdfe6
Documenter.jl <documenter@juliadocs.github.io>
2026-02-27 12:27:16 +0000

    build based on 06b56c4

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#HeteroTomo3D","page":"Home","title":"HeteroTomo3D","text":"Documentation for HeteroTomo3D.\n\n","category":"section"}]
+[{"location":"#HeteroTomo3D","page":"Home","title":"HeteroTomo3D","text":"Documentation for HeteroTomo3D.\n\n","category":"section"},{"location":"#HeteroTomo3D.dummy_volume-Tuple{Any, Any, Any}","page":"Home","title":"HeteroTomo3D.dummy_volume","text":"dummy_volume(width, height, depth)\n\nCalculates the volume of a simple 3D bounding box.\n\n\n\n\n\n","category":"method"}]
 }
